69ddc81d3c6356c6530a9e37d7919925ac204c21
Anudeep Reddy Bondugula <anudeepreddybondugula@gmail.com>
2023-05-01 12:16:46 +0530

    Removed some unwanted React files

--- src/utils/ECDSA.js
+++ src/utils/ECDSA.js
@@ -11,21 +11,3 @@ export default function getPublicKey(privateKey){
     return publicKey;
 
 }
-
-// Generate a new Ethereum private key
-// const privateKey = 'f8f8a2f43c8376ccb0871305060d7b27b0554d2cc72bccf41b2705608452f315';
-
-// // Initialize the elliptic curve object
-// const secp256k1 = new ec('secp256k1');
-
-// // Generate the public key
-// const keyPair = secp256k1.keyFromPrivate(privateKey, 'hex');
-// const publicKey = keyPair.getPublic().encode('hex');
-
-// // Generate the wallet address
-// const publicKeyBuffer = Buffer.from(publicKey.substring(2), 'hex');
-// const hash = keccak256(publicKeyBuffer);
-// const address = '0x' + hash.substring(24);
-
-// console.log('Public key:', publicKey);
-// console.log('Wallet address:', address);
