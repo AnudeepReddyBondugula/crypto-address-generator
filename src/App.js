a86c0184261c88bf9e822288ad4fe54dff30f89f
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-08-27 18:27:37 +0530

    bg changed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Form from './Form.js'
 function App() {
   return (
     <div className="wrapper">
-      <div className="title">CRYPTO ADDRESS GENERATOR</div>
+      <div className="title">CRYPTO ADDRESS GENERATOR
         <Form />
+      </div>
     </div>
   );
 }
